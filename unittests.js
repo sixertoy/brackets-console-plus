baaa3e99eadf33918fd1e074dc9a342afbc43174
malas34 <malas34.github@gmail.com>
2014-09-15 11:02:14 +0200

    Add Array + Object (no circular) debug + icon + warns notifier

---
+++ unittests.js
@@ -0,0 +1,8 @@
+/*jslint vars: true, plusplus: true, devel: true, browser: true, nomen: true, indent: 4 */
+/*global define, brackets, describe, it, expect, beforeEach, afterEach */
+define(function (require, exports, module) {
+    'use strict';
+
+    require('unittests/main.test');
+
+});
